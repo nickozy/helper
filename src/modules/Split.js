b041a8fa60c62927a5f48e38c66aebab257a650b
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-30 02:07:30 +0300

    split type dur 0.6

--- src/modules/Split.js
+++ src/modules/Split.js
@@ -24,7 +24,7 @@ console.log("color")
 gsap.from(elem.words, {
     opacity: 0,
     y: '50%',
-    ease: 'power2.out',
+    ease: 'cubic-bezier(0.23, 0.72, 0.59, 0.95)',
     rotate: 2,
     duration: 0.6,
     color: "transparent",
