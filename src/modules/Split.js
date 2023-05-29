4cd8746381399afdbeaa68d1f6a3ea7ceb3d1670
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-30 01:32:34 +0300

    split type dur 0.6

--- src/modules/Split.js
+++ src/modules/Split.js
@@ -15,13 +15,13 @@ export default class extends module {
 let elem = new SplitType( el1, { types: 'words, chars' })
 let elem2 = new SplitType( el2, { types: 'words, chars' })
 
-
+console.log("split")
 gsap.from(elem.chars, {
     opacity: 0,
     y: '50%',
     ease: 'power1.out',
     rotate: 2,
-    duration: 0.6,
+    duration: 1,
     stagger: { amount: 0.3 },
     })
 gsap.from(elem2.chars, {
@@ -29,7 +29,7 @@ gsap.from(elem2.chars, {
     y: '50%',
     ease: 'power4.out',
     rotate: 2,
-    duration: 0.6,
+    duration: 1,
     stagger: { amount: 0.3 },
     })
    }   
