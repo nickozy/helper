5607cb73b00d89d9a009e9d0b9348d1bd0f60f12
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-30 01:16:43 +0300

    split type

--- src/modules/Split.js
+++ src/modules/Split.js
@@ -1,6 +1,7 @@
 import { module } from 'modujs';
 import SplitType from 'split-type';
 import gsap from 'gsap';
+import { EasePack } from 'gsap/all';
 
 
 export default class extends module {
@@ -16,7 +17,7 @@ let elem = new SplitType( this.el, { types: 'words, chars' })
 gsap.from(elem.chars, {
     opacity: 0,
     y: '50%',
-    ease: ease,
+    ease: 'power2.out',
     rotate: 2,
     duration: 0.3,
     stagger: { amount: 0.3 },
