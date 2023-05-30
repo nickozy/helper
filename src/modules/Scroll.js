4d004c5644675fd8bcc9af070c3842078b45c823
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-31 01:55:35 +0300

    text blur

--- src/modules/Scroll.js
+++ src/modules/Scroll.js
@@ -18,7 +18,7 @@ export default class extends module {
             el: this.el,
             getDirection: true,
             smooth: true,
-            multiplier: 1,
+            multiplier: 0.8,
             lerp: 0.07,
         });
 
